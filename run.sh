2ca5db99e4cf9b962e43c4457d21e84df13e2374
Yasutaka Kawamoto <kwmt27@gmail.com>
2016-12-25 15:54:20 +0900

    fix

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 
-export GOPATH="$WERCKER_SETUP_BUONO_GOPATH"
+export GOPATH="$WERCKER_SOURCE_DIR/$WERCKER_SETUP_BUONO_GOPATH"
 
 info "\$GOPATH now points to: $GOPATH"
 success "Go workspace setup finished"
